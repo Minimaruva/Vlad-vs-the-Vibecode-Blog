0668169c769dd3ec0075c345e76d7194c26d08ca
Vlad Y <vladjagnion@gmail.com>
2026-02-19 17:29:07 +0000

    basic version of blog after tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://singular-axolotl-e5d19f.netlify.app/"
+  site: "https://singular-axolotl-e5d19f.netlify.app/",
+  integrations: [preact()]
 });
