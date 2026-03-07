d231ce907089bcb2b0f2642b387f266b22e3aef6
Vlad Y <vladjagnion@gmail.com>
2026-03-07 18:15:32 +0000

    draggable window

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -4,3 +4,46 @@ menu?.addEventListener('click', () => {
     const isExpanded = menu.getAttribute('aria-expanded') === 'true';
     menu.setAttribute('aria-expanded', `${!isExpanded}`);
 });
+
+// Draggable windows
+const titleBar = document.querySelector(".title-bar");
+const win = document.querySelector(".desktop-window");
+
+if (titleBar && win) {
+  dragElement(win, titleBar);
+}
+
+function dragElement(win, handle) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  handle.onmousedown = dragMouseDown;
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the window's new position:
+    win.style.top = (win.offsetTop - pos2) + "px";
+    win.style.left = (win.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
